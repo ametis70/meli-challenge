9fabf0e509a740a0512c1f7b204bf8860a03b7be
Ian Mancini <ianmethyst@gmail.com>
2021-12-21 23:58:13 -0300

    chore: Use CORS in development

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,30 @@ import preRender from './server/renderer'
 
 const app = express()
 
-app.use('/api', apiRouter)
-app.get('*', preRender)
-
-let port = 3000
-if (process.env.PORT !== undefined) {
-  const _port = parseInt(process.env.PORT)
-  if (isNaN(_port)) {
-    console.warn(
-      `PORT env var (${process.env.PORT}) is not a number, falling back to port 3000`,
-    )
-  } else {
-    port = _port
+const start = async () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { default: cors } = await import('cors')
+    app.use(cors())
   }
+
+  app.use('/api', apiRouter)
+  app.get('*', preRender)
+
+  let port = 3000
+  if (process.env.PORT !== undefined) {
+    const _port = parseInt(process.env.PORT)
+    if (isNaN(_port)) {
+      console.warn(
+        `PORT env var (${process.env.PORT}) is not a number, falling back to port 3000`,
+      )
+    } else {
+      port = _port
+    }
+  }
+
+  app.listen(port, () => {
+    console.info(`Server is running on port ${port}`)
+  })
 }
 
-app.listen(port, () => {
-  console.info(`Server is running on port ${port}`)
-})
+start()
