5470e3f1072fb31c7359044d0b4b29416312531f
Ian Mancini <ianmethyst@gmail.com>
2021-12-21 23:37:58 -0300

    feat: Add client entry point

---
+++ src/client.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { createBroswerContext } from 'use-sse'
+
+import App from './components/App'
+
+const BroswerDataContext = createBroswerContext()
+
+ReactDOM.hydrate(
+  <React.StrictMode>
+    <BroswerDataContext>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </BroswerDataContext>
+  </React.StrictMode>,
+  document.getElementById('app'),
+)
