d7671f102137f192f19990ee4be5690f0f5a2afd
Ian Mancini <ianmethyst@gmail.com>
2021-12-22 15:59:37 -0300

    fix: Disable `url()` handling with `style-loader`

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,16 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+          'sass-loader',
+        ],
       },
     ],
   },
