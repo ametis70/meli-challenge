3b033ef3010a3d58fd7b3b507477fe4f4c7df6c1
Ian Mancini <ianmethyst@gmail.com>
2021-12-22 17:20:42 -0300

    feat: Store search query and reset it on `/` view

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useRef } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
 const Search: React.VFC = () => {
+  const [searchParams] = useSearchParams()
+  const searchValue = searchParams.get('search')
+  const ref = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (ref.current && !searchValue) {
+      ref.current.value = ''
+    }
+  }, [searchValue, ref])
+
   return (
     <form method="get" action="/items" className="search-container">
       <input
+        ref={ref}
         id="search-box"
         type="text"
         name="search"
         aria-label="Campo de búsqueda"
         placeholder="Nunca dejes de buscar"
+        value={searchValue ? decodeURI(searchValue) : undefined}
       />
       <label htmlFor="search-button" aria-hidden="true" className="search-button" />
       <input id="search-button" hidden type="submit" value="Buscar" />
